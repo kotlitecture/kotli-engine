0b00873f1e16fd10e046426e8807e818e9cf5abe
Aleksha <kotlitecture@gmail.com>
2024-02-24 12:32:12 +0200

    fix (delete empty folders logic)

--- engine/src/main/kotlin/kotli/engine/utils/PathUtils.kt
+++ engine/src/main/kotlin/kotli/engine/utils/PathUtils.kt
@@ -6,7 +6,12 @@ import org.slf4j.LoggerFactory
 import java.nio.file.FileSystems
 import java.nio.file.Files
 import java.nio.file.Path
-import kotlin.io.path.*
+import kotlin.io.path.ExperimentalPathApi
+import kotlin.io.path.copyToRecursively
+import kotlin.io.path.deleteIfExists
+import kotlin.io.path.deleteRecursively
+import kotlin.io.path.isDirectory
+import kotlin.io.path.pathString
 
 /**
  * Common utilities for operations with paths.
@@ -25,12 +30,12 @@ object PathUtils {
     fun getFromResource(resource: String): Path? {
         val fromUri = javaClass.getResource("/$resource")?.toURI() ?: return null
         val path = runCatching { Path.of(fromUri) }
-                .onFailure { logger.error("getFromResource", it) }
-                .getOrNull()
-                ?: run {
-                    val fs = FileSystems.newFileSystem(fromUri, emptyMap<String, String>())
-                    fs.getPath(resource)
-                }
+            .onFailure { logger.error("getFromResource", it) }
+            .getOrNull()
+            ?: run {
+                val fs = FileSystems.newFileSystem(fromUri, emptyMap<String, String>())
+                fs.getPath(resource)
+            }
         logger.debug("getFromResource :: {} -> {}", fromUri, path)
         return path
     }
@@ -45,12 +50,12 @@ object PathUtils {
         logger.debug("copy :: {} -> {}", from, to)
         val followLinks = true
         from.copyToRecursively(
-                to,
-                followLinks = followLinks,
-                copyAction = { src, dst ->
-                    val toDst = dst.resolve(dst.pathString.replace("\\", "/"))
-                    src.copyToIgnoringExistingDirectory(toDst, followLinks)
-                }
+            to,
+            followLinks = followLinks,
+            copyAction = { src, dst ->
+                val toDst = dst.resolve(dst.pathString.replace("\\", "/"))
+                src.copyToIgnoringExistingDirectory(toDst, followLinks)
+            }
         )
     }
 
@@ -61,7 +66,7 @@ object PathUtils {
      * @return True if the path is an empty directory; otherwise, false.
      */
     fun isEmptyDir(path: Path): Boolean {
-        return path.isDirectory() && Files.list(path).count() == 0L
+        return path.isDirectory() && Files.walk(path).noneMatch { Files.isRegularFile(it) }
     }
 
     /**
@@ -71,7 +76,7 @@ object PathUtils {
      */
     fun delete(path: Path) {
         logger.debug("delete :: {}", path)
-        path.deleteIfExists()
+        runCatching { path.deleteRecursively() }
     }
 
 }
